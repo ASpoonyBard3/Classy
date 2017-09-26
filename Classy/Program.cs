f542d3e9fea55faac6f3fd963707467eb553540f
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-26 19:36:21 +0100

    Gettting classy with cars.

--- Classy/Program.cs
+++ Classy/Program.cs
@@ -10,6 +10,26 @@ namespace Classy
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            myCar.Make = "OldsMobile";
+            myCar.Model = "Cutlass Supreme";
+            myCar.Year = 1985;
+            myCar.Colour = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Colour);
+            Console.ReadLine();
         }
     }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+    }
+
 }
